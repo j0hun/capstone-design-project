ce005b17c32afd7490a1919a26d7022d8362ab36
j0hun <dudgns3681@gmail.com>
2024-03-20 16:35:45 +0900

    Initial Commit

---
+++ src/test/java/com/inu/capstone/graduationproject/GraduationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inu.capstone.graduationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
